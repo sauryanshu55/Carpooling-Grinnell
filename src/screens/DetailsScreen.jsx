b65c922ea670064d6e860aaa3ad5e4a0c53c3613
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2024-07-11 13:19:31 -0500

    Added a DateTime Picker

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import {
     View,
     Text,
     Button
 } from 'react-native'
-
+import DatePicker from 'react-native-date-picker'
 
 export function DetailsScreen({navigation}){
+
+    const [date,setDate]=useState(new Date())
+
     return (
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>Ride Details</Text>
-        </View>
+        <Text>
+            {/* Date and Time */}
+            <DatePicker date={date} onDateChange={setDate}/>    
+            <Text>{date.toTimeString()}</Text>
+        </Text>
       );
   }
   
