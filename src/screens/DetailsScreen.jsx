f5b1b176fa6269018db4765615cf359d678df8cb
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2024-07-11 22:10:20 -0500

    Added a few form elements to the details screen
    Added a Flexibility indicating toggle button, and a by Flexible numerical input

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react';
-import {
-    View,
-    Text,
-    Button
-} from 'react-native'
+import {View,Text,Button,Switch, TextInput} from 'react-native'
 import DatePicker from 'react-native-date-picker'
 
 export function DetailsScreen({navigation}){
 
     const [date,setDate]=useState(new Date())
+    const [isFlexible,setIsFlexible]=useState(false)
+    const toggleIsFlexibleSwitch = () => setIsFlexible(previousState => !previousState);
+    const [byFlexible, setByFlexible] = useState(""); 
+    const handleByFlexibleTimeChange = (text) => { setByFlexible(text.replace(/[^0-9]/g, ""))}; 
 
+    
     return (
-        <Text>
+        <View>
             {/* Date and Time */}
             <DatePicker date={date} onDateChange={setDate}/>    
-            <Text>{date.toTimeString()}</Text>
-        </Text>
-      );
-  }
+
+            {/* Flexible Checkbox */}
+            <Switch value={isFlexible} onValueChange={toggleIsFlexibleSwitch}/>
+
+            {/* Flexible By */}
+            {isFlexible ? (
+                <TextInput 
+                    onChangeText={handleByFlexibleTimeChange} value={byFlexible} keyboardType="numeric" placeholder="15 mins"
+                />
+            ) : null}        
+        </View>
+    );
+}
   
