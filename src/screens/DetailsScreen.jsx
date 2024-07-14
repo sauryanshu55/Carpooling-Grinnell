9b6301bbafade6ede7f07b51b5e7c50283508c52
sauryanshu55 <95371800+sauryanshu55@users.noreply.github.com>
2024-07-14 13:33:40 -0500

    Converted to react-native paper
    Made the details screen look a bit better

--- src/screens/DetailsScreen.jsx
+++ src/screens/DetailsScreen.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import {View,Text,Button,Switch, TextInput} from 'react-native'
+import { View, StyleSheet } from 'react-native';
 import DatePicker from 'react-native-date-picker'
 import { generateClient } from 'aws-amplify/api';
 import { createRideDetails } from '../graphql/mutations';
+import { Button, Switch, TextInput, Title, Subheading } from 'react-native-paper';
 
 
 export function DetailsScreen({navigation}){
@@ -40,24 +41,87 @@ export function DetailsScreen({navigation}){
     
     // RETURN COMPONENT
     return (
-        <View>
-            {/* Date and Time */}
-            <DatePicker date={date} onDateChange={setDate}/>    
-
-            {/* Flexible Checkbox */}
-            <Switch value={isFlexible} onValueChange={toggleIsFlexibleSwitch}/>
-
-            {/* Flexible By */}
-            {isFlexible && (
-                <TextInput 
-                    onChangeText={handleByFlexibleTimeChange} value={byFlexible} keyboardType="numeric" placeholder="15 mins"
-                />
-            )}
-
-            {/* Reuest Button */}
-            <Button title='Request Ride' onPress={save}/>
-
+        <View style={styles.container}>
+          <Title style={styles.title}>Request a Ride</Title>
+    
+          {/* Date and Time */}
+          <View style={styles.formGroup}>
+            <Subheading>Select a Date and Time</Subheading>
+            <DatePicker
+              date={date}
+              onDateChange={setDate}
+              mode="datetime"
+            />
+          </View>
+    
+          {/* Flexible Checkbox */}
+          <View style={styles.formGroup}>
+            <Subheading>Are you flexible with the time you're requesting?</Subheading>
+            <View style={styles.switchContainer}>
+              <Switch value={isFlexible} onValueChange={toggleIsFlexibleSwitch} />
+              <Subheading style={styles.switchLabel}>
+                {isFlexible ? 'Yes' : 'No'}
+              </Subheading>
+            </View>
+          </View>
+    
+          {/* Flexible By */}
+          {isFlexible && (
+            <View style={styles.formGroup}>
+              <Subheading>By how much are you flexible?</Subheading>
+              <TextInput
+                mode="outlined"
+                onChangeText={handleByFlexibleTimeChange}
+                value={byFlexible}
+                keyboardType="numeric"
+                placeholder=" 15 mins"
+                style={styles.input}
+              />
+            </View>
+          )}
+    
+          {/* Request Button */}
+          <Button
+            mode="contained"
+            onPress={save}
+            style={styles.button}
+            labelStyle={styles.buttonLabel}
+          >
+            Request Ride
+          </Button>
         </View>
-    );
+      );
 }
   
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 16,
+      backgroundColor: '#f5f5f5',
+    },
+    title: {
+      marginBottom: 20,
+      textAlign: 'center',
+    },
+    formGroup: {
+      marginBottom: 20,
+    },
+    switchContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 8,
+    },
+    switchLabel: {
+      marginLeft: 8,
+    },
+    input: {
+      marginTop: 8,
+    },
+    button: {
+      marginTop: 20,
+      paddingVertical: 8,
+    },
+    buttonLabel: {
+      fontSize: 16,
+    },
+  });
